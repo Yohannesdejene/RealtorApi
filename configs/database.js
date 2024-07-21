38b10770fb632d89957e6b95d078cafea62cf7c3
johnabi <81425097+Yohannesdejene@users.noreply.github.com>
2024-07-21 21:00:52 +0300

    Update database config

--- configs/database.js
+++ configs/database.js
@@ -1,12 +1,9 @@
 const Sequelize = require("sequelize").Sequelize;
 
-// const sequelize = new Sequelize("circlefr_realtor", "root", "",
-//     { host: "localhost", dialect: "mysql" }
-// );
+const sequelize = new Sequelize("circlefr_realtor", "root", "",
+    { host: "localhost", dialect: "mysql" }
+);
 
-const sequelize = new Sequelize("circlefr_realtor", "root", "", {
-  dialect: "mysql",
-  host: "localhost",
-});
+
 
 module.exports = sequelize;
